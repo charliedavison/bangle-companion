3b16186e46ceabeb7a6ac80f2e6461a5f8fef0e6
Charles Davison <charlesdavison@gmail.com>
2020-04-18 17:41:04 +0100

    Custom service test

--- src/providers/ConnectionProvider.tsx
+++ src/providers/ConnectionProvider.tsx
@@ -24,10 +24,17 @@ let bluetoothDevice: BluetoothDevice;
 let bluetoothCharacteristic: BluetoothRemoteGATTCharacteristic;
 
 export const ConnectionProvider = ({ children }: ConnectionProviderProps) => {
-    const bluetoothServiceUUID: BluetoothServiceUUID = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
-    const bluetoothCharacteristicUUID: BluetoothCharacteristicUUID = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
+    // const bluetoothServiceUUID: BluetoothServiceUUID = '6e400001-b5a3-f393-e0a9-e50e24dcca9e';
+    // const bluetoothCharacteristicUUID: BluetoothCharacteristicUUID = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
+    
+    // const bluetoothServiceUUID: BluetoothServiceUUID = '92c1b5b8-e824-4938-bbed-672529103e56';
+    // const bluetoothCharacteristicUUID: BluetoothCharacteristicUUID = '92c1b5b8-e825-4938-bbed-672529103e56';
 
-    const [isDeviceConnected, setDeviceConnected] = React.useState<boolean>(false);
+    const bluetoothServiceUUID: BluetoothServiceUUID = 'f8b23a4d-89ad-4220-8c9f-d81756009f0c';
+    const bluetoothCharacteristicUUID: BluetoothCharacteristicUUID = 'f8b23a4d-89ad-4220-8c9f-d81756009f0c';
+
+
+    const [isDeviceConnected, setIsDeviceConnected] = React.useState<boolean>(false);
     const [isDeviceConnecting, setIsDeviceConnecting] = React.useState<boolean>(false);
 
     const getConnectionFilter = (): RequestDeviceOptions => ({
@@ -40,6 +47,10 @@ export const ConnectionProvider = ({ children }: ConnectionProviderProps) => {
         return await navigator.bluetooth.requestDevice(filter);
     }
 
+    const changedEvent = (e: any) => {
+        console.log(e);
+    }
+
     const connect = async (): Promise<void> => {
         try {
             setIsDeviceConnecting(true);
@@ -47,19 +58,34 @@ export const ConnectionProvider = ({ children }: ConnectionProviderProps) => {
             const bluetoothDevice = await requestDevice();
 
             bluetoothDevice.addEventListener('gattserverdisconnected', () => {
-                setDeviceConnected(false)
+                setIsDeviceConnected(false)
             })
-
+            
             const bluetoothGATTServer = await bluetoothDevice.gatt?.connect();
+
+            // const services = await bluetoothGATTServer?.getPrimaryServices();
+            // console.log(services);
+
             const bluetoothGATTService = await bluetoothGATTServer?.getPrimaryService(bluetoothServiceUUID);
             bluetoothCharacteristic = await bluetoothGATTService!.getCharacteristic(bluetoothCharacteristicUUID);
-            setDeviceConnected(true);
+            
+            debugger;
+            bluetoothCharacteristic.addEventListener('characteristicvaluechanged', changedEvent);
+            bluetoothCharacteristic.startNotifications();
+
+            const v = await bluetoothCharacteristic.readValue();
+            console.log(`Read value: ${v}`);
+
+            setIsDeviceConnected(true);
             setIsDeviceConnecting(false);
+
+
 
             // Write a buzz to the watch to test connection.
             await write('Bangle.buzz();')
         } catch (err) {
             setIsDeviceConnecting(false);
+            console.error(err);
         }
     }
 
@@ -73,6 +99,8 @@ export const ConnectionProvider = ({ children }: ConnectionProviderProps) => {
         await bluetoothCharacteristic?.writeValue(buffer);
         console.log(`Written ${val} to watch`);
     }
+
+
 
     return (
         <ConnectionContext.Provider value={{ connect, disconnect, write, isDeviceConnected, isDeviceConnecting }}>
